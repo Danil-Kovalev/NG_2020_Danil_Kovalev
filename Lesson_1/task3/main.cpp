b747a6c7be4a344da6346baa84268739a292aa18
danil <danilkovalev1306@gmail.com>
2020-11-11 15:40:55 +0200

    Lesson_1/Task1

---
+++ Lesson_1/task3/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 0;
+int b = 0;
+int c = 0;
+int d = 0;
+cout << "Skolko oreshkov nuzhno belochke: ";
+cin >> a;
+cout << "Skolko oreshkov v odnoy shishke: ";
+cin >> b;
+cout << "Skolko shishek bylo sobrano: ";
+cin >> c;
+d = b * c;
+if ( a <= d)
+{
+cout << "Yes";
+}
+if (a > d)
+{
+cout << "No. Zhalko belochku(";
+}
+}
