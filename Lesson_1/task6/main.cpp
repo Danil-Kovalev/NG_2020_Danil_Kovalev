c3983d21956d1fa6d5d9b80040378f03c27aef9a
danil <danilkovalev1306@gmail.com>
2020-11-14 22:07:16 +0200

    task6

--- Lesson_1/task6/main.cpp
+++ Lesson_1/task6/main.cpp
@@ -4,20 +4,47 @@ using namespace std;
 
 int main()
 {
- int a, b ,c;
+ int a, b ,c, d;
  cout << " Enter size: ";
  cin >> a;
- for ( b = 0; b <= a - 1; b++ ) {
-   for ( c = 0; c <= a - 2; c++)  {
-       cout << "*";
-   }
-     cout << "*" << endl;
-   }
- cout << endl;
- for ( b = 0; b <= a - ( a - 1 ); b++) {
-     cout << "*" << endl;
-     for ( b = 0; b <= a - ( a - 2 ); b++) {
+ for ( b = 0; b < a; b++) {   //kvadrat
+     for (c = 0; c < a; c++) {
          cout << "*";
      }
+     cout << endl;
+ }
+ cout << endl; //1 treugolnik
+ for (b = 0; b < a; b++) {
+     for (c = 0; c <= b; c++) {
+         cout << "*";
+     }
+     cout << endl;
+ }
+ cout << endl; //2 treugolnik
+ for (b = 0; b < a; b++) {
+     for (c = a - 1; c > b; c--) {
+         cout << " ";
+     }
+     for (c = 0; c <= b; c++) {
+         cout << "*";
+     }
+     cout << endl;
+ }
+ cout << endl; //3 treugolnik
+ for (b = 0; b < a; b++) {
+     for (c = a; c > b; c--) {
+         cout << "*";
+     }
+     cout << endl;
+ }
+ cout << endl; //4 treugolnik
+ for (b = 0; b < a; b++) {
+     for (c = 0; c <= b; c++) {
+         cout << " ";
+     }
+     for (c = a; c > b; c--) {
+         cout << "*";
+     }
+     cout << endl;
  }
 }
