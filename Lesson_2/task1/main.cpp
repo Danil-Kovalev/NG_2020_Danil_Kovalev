6f4d37d3c0129d4343c0223578caa3f892781e26
danil <danilkovalev1306@gmail.com>
2020-11-18 23:47:57 +0200

    Lesson_2/task1

---
+++ Lesson_2/task1/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr [11];
+    cout << "Enter number school:";
+    cin >> arr [0];
+    cout << "Enter number school:";
+    cin >> arr [1];
+    cout << "Enter number school:";
+    cin >> arr [2];
+    cout << "Enter number school:";
+    cin >> arr [3];
+    cout << "Enter number school:";
+    cin >> arr [4];
+    cout << "Enter number school:";
+    cin >> arr [5];
+    cout << "Enter number school:";
+    cin >> arr [6];
+    cout << "Enter number school:";
+    cin >> arr [7];
+    cout << "Enter number school:";
+    cin >> arr [8];
+    cout << "Enter number school:";
+    cin >> arr [9];
+    cout << "Enter number your school:";
+    cin >> arr [10];
+    for (int i = 0; i < arr [11]; i++) {
+        if (i == 11) {
+           cout << "I know it`s school!";
+        }
+    }
+}
