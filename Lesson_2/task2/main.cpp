407b43b1e3997eed47802fff1be2ac172fba17e1
danil <danilkovalev1306@gmail.com>
2020-11-28 22:17:44 +0200

    Lesson_2/task2

--- Lesson_2/task2/main.cpp
+++ Lesson_2/task2/main.cpp
@@ -4,72 +4,35 @@ using namespace std;
 
 int main()
 {
-  int arr [10] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  int money [10];
-  for (int i = 0; i < arr [9]; i++) {
-      cout << "Enter number card:";
-      cin >> arr [i];
-      switch (i < arr [i]) {
-      case 1:
-             cout << "How much do you want to put on the card?:";
-             cin >> money [0];
-             cout << "Sum on all cards:" << arr [10]++;
-             break;
-
-  case 2:
-         cout << "How much do you want to put on the card?:";
-         cin >> money [1];
-         cout << arr [10];
-         cout << "Sum on all cards:" << arr [10]++;
-         break;
-  case 3:
-         cout << "How much do you want to put on the card?:";
-         cin >> money [2];
-         cout << arr [10];
-         cout << "Sum on all cards:" << arr [10]++;
-         break;
-  case 4:
-         cout << "How much do you want to put on the card?:";
-         cin >> money [3];
-         cout << arr [10];
-         cout << "Sum on all cards:" << arr [10]++;
-         break;
-  case 5:
-         cout << "How much do you want to put on the card?:";
-         cin >> money [4];
-         cout << arr [10];
-         cout << "Sum on all cards:" << arr [10]++;
-         break;
-  case 6:
-         cout << "How much do you want to put on the card?:";
-         cin >> money [5];
-         cout << arr [10];
-         cout << "Sum on all cards:" << arr [10]++;
-         break;
-  case 7:
-         cout << "How much do you want to put on the card?:";
-         cin >> money [6];
-         cout << arr [10];
-         cout << "Sum on all cards:" << arr [10]++;
-         break;
-  case 8:
-         cout << "How much do you want to put on the card?:";
-         cin >> money [7];
-         cout << arr [10];
-         cout << "Sum on all cards:" << arr [10]++;
-         break;
-  case 9:
-         cout << "How much do you want to put on the card?:";
-         cin >> money [8];
-         cout << arr [10];
-         cout << "Sum on all cards:" << arr [10]++;
-         break;
-  case 10:
-         cout << "How much do you want to put on the card?:";
-         cin >> money [9];
-         cout << arr [10];
-         cout << "Sum on all cards:" << arr [10]++;
-         break;
-  }
-  }
-  }
+    int arr [10];
+    int num = 0;
+    int money = 0;
+    int sum = 0;
+    char ex;
+    for (int i = 0; i< 10; i ++)
+    {
+        arr [i] = 0;
+    }
+    while (true)
+    {
+        cout << "Enter credit card number:" << endl;
+        cin >> num;
+        cout << "How much money do you want to put on the card?" << endl;
+        cin >> money;
+        sum += money;
+        arr [num-1] += money;
+        for (int i = 0; i < 10; i++)
+        {
+            cout << arr [i] << " ";
+        }
+        cout << endl;
+        cout << "The amount of the money on all cards: " << sum << endl;
+        cout << "Press F for exit" << endl;
+        cin >> ex;
+        if (ex == 'F') {
+           break;
+        }
+        cout << endl;
+    }
+    return 0;
+}
