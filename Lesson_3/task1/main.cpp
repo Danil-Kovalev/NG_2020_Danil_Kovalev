b74806b881adbb16324ae4a3cbc2e303f1874ac6
danil <danilkovalev1306@gmail.com>
2020-12-19 21:45:34 +0200

    Lesson_3

--- Lesson_3/task1/main.cpp
+++ Lesson_3/task1/main.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
 #include <string.h>
 
+
 using namespace std;
 
 int main()
 {
-    char str [20];
-    cout << "Enter string: ";
-    cin >> str;
-    while (str != '\0') {
-          str++;
-    }
-
-    cout << str;
+ char str[500];
+ int count = 0;
+ int word = 0;
+ int i = 0;
+ cout << "Enter word: ";
+ cin.get(str, 500);
+ while (str[i] != '\0') {
+       if (str[i] != ' ' && word == 0)
+       {
+          word = 1;
+          count++;
+       }
+       else if (str[i] == ' ')
+               word = 0;
+               i++;
+       }
+ cout << "Number words" << count;
 }
