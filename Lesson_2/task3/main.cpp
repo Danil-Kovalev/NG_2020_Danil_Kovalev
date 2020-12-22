c35e48d7d4b990544ce2ca74ed216919a0d02a64
danil <danilkovalev1306@gmail.com>
2020-12-22 17:01:37 +0200

    Lesson_2/task3

--- Lesson_2/task3/main.cpp
+++ Lesson_2/task3/main.cpp
@@ -5,29 +5,29 @@ using namespace std;
 int main()
 {
  int arr[20];
-    int str = 0, max = 0;
-    for(int i = 0; i < 20; i++)
-    {
-        cout << "Enter the number: ";
-        cin >> arr[i];
-        if (max < arr[i])
-            max = arr[i];
-        str++;
-        if(arr[i] == 0) {
-            break;
-        }
-    }
-    for (int i = 0; i < str; i++)
-    {
-        for (int q = 0; q < max - (arr[i] + q); q++)
-        {
-            cout << " ";
-        }
-        for (int q = 0; q != arr[i]; q++)
-        {
-            cout << "*";
-        }
-        cout << endl;
-    }
+ int str = 0, max = 0;
+ for (int i = 0; i < 20; i++)
+ {
+     cout << "Enter the number: ";
+     cin >> arr[i];
+     if (max < arr[i])
+        max = arr[i];
+     str++;
+     if (arr[i] == 0) {
+        break;
+     }
+ }
+ for (int i = 0; i < str; i++)
+ {
+     for (int q = 0; q < max - (arr[i] + q); q++)
+     {
+         cout << " ";
+     }
+     for (int q = 0; q != arr[i]; q++)
+     {
+         cout << "*";
+     }
+     cout << endl;
+ }
 }
 
