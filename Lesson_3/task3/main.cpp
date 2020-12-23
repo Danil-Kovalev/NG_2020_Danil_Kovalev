053b1c70d7546b57f9b9c96229d4135788e70c0a
danil <danilkovalev1306@gmail.com>
2020-12-23 19:44:56 +0200

    Lesson_3/task3

---
+++ Lesson_3/task3/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cctype>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    char string[500];
+    int i = 0;
+    cout << "Enter string: ";
+    cin.get (string, 500);
+    cout << "Your string: ";
+    for (i = 0; i < string[i]; i++) {
+        cout << (char)toupper(string[i]);
+    }
+}
