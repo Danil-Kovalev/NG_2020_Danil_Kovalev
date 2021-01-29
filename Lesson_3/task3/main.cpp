8e98711786ea2676f8a90bcfd2cfd80fcc5de3e8
danil <danilkovalev1306@gmail.com>
2021-01-29 19:34:25 +0200

    add Lesson_3/task3

--- Lesson_3/task3/main.cpp
+++ Lesson_3/task3/main.cpp
@@ -6,12 +6,16 @@ using namespace std;
 int main()
 {
     char string[500];
+    char tempo;
     int i = 0;
     cout << "Enter string: ";
     cin.get (string, 500);
+    cout << "Your string: ";
     for (i = 0; i < string[i]; i++) {
-        if (string [i] >= 'a' && string [i] <= 'z')
-           string [i] -= 32;
+        if (string [i] >= 'a' && string [i] <= 'z') {
+           tempo = string [i];
+           tempo -= 32;
+           cout << tempo;
+        }
     }
-    cout << "Your string: " << string;
 }
