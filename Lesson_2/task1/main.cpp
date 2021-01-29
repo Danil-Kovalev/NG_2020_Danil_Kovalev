821aae68ba2020870df70291cac088599430d846
danil <danilkovalev1306@gmail.com>
2021-01-29 19:49:17 +0200

    add Lesson_2/task1

--- Lesson_2/task1/main.cpp
+++ Lesson_2/task1/main.cpp
@@ -5,19 +5,23 @@ using namespace std;
 int main()
 {
  int i = 0;
- int sch = 0;
+ int mySchool = 0;
  int arr [10];
  for (i = 0; i < 10; i++) {
      cout << "Enter number school:";
      cin >> arr [i];
  }
  cout << "Enter your school, please:";
- cin >> sch;
+ cin >> mySchool;
+ bool boolean;
  for (i = 0; i < 10; i++) {
-     if (arr [i] == sch) {
-        cout << "I know your school!";
-        return 0;
+     if (mySchool == arr [i]) {
+        boolean = true;
      }
-}
- cout << "I don`t know your school(";
+ }
+ if (boolean == true) {
+    cout << "I know your school!";
+ }
+ else
+    cout << "I don`t know your school(";
 }
