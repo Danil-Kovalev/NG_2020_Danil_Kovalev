b683d38c1b4041b39ca13f834512c35592f00ce1
danil <danilkovalev1306@gmail.com>
2021-01-30 18:05:25 +0200

    add Lesson_4/task3

--- Lesson_4/task3/main.cpp
+++ Lesson_4/task3/main.cpp
@@ -2,38 +2,39 @@
 
 using namespace std;
 
-void getInputUser (char string[]) {
-    cout << "Enter words: ";
-    cin.get(string, 500);
+void getInputUser(char string[]) {
+     cout << "Enter words: ";
+     cin.get(string, 500);
 }
-
-void textOutput (int count) {
+void textOutput(int count) {
      cout << "Number words: " << count;
 }
-
-void wordCountCycle (char string[500]) {
+int wordCount(char string[500]) {
     int word = 0;
     int i = 0;
     int count = 0;
-    while (string [i] != '\0') {
-       if (string [i] != ' ' && word == 0)
-       {
-          if (string [i] >= 'a' && string [i] <= 'z') {
-             word = 1;
-             count++;
+    while (string[i] != '\0') {
+          if (string[i] != ' ' && word == 0)
+          {
+             if (string[i] >= 'a' && string[i] <= 'z') {
+                word = 1;
+                count++;
+             }
           }
-       }
-       else if (string [i] >= '0' && string [i] <= '9')
-               word = 0;
-       i++;
- }
- textOutput(count);
+          else if (string[i] >= '0' && string[i] <= '9')
+                  word = 0;
+          i++;
+    }
+    return count;
 }
-
+void wordCountCycle(char string[500]) {
+    int amount;
+    amount = wordCount(string);
+    textOutput(amount);
+}
 int main()
 {
  char string[500];
- int count = 0;
  getInputUser(string);
  wordCountCycle(string);
 }
